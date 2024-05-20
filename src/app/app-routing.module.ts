bd60f97cae3ef4bf98739f1e6b692327db46515b
Namrata1998 <namrata.khade@neutrinotechlabs.com>
2024-05-20 17:28:27 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { NoPageComponent } from './no-page/no-page.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'about',
+    component:AboutComponent,
+    children:[
+      {
+        path:'company',
+        component:AboutCompanyComponent
+      },
+      {
+        path:'me',
+        component:AboutMeComponent
+      },
+    ]
+  },
+  {
+    path:'user/:id',
+    component:UserComponent
+  },
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'**',
+    component:NoPageComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
